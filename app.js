0e37dcabb3b7facf777e667b12229b704af32aad
Andrea Gallo <andreagallo264@gmail.com>
2021-01-20 15:40:10 -0300

    Express instaled, server up

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res)=>{
+    res.send("Welcome");
+});
+
+app.listen(8080, () => console.log("connected to port 8080"));
